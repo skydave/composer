47bdb3963a82399a6f21aebc50b06b6f74bbab9b
David Koerner <dk402538@googlemail.com>
2012-04-01 22:33:07 +0200

    updated server code

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,8 @@
 #include <gfx/glsl/common.h>
 #include <gfx/FBO.h>
 
+#include <tinythread/tinythread.h>
+
 
 
 #include <3rdparty\ip\TcpSocket.h>
@@ -68,14 +70,96 @@ void shutdown()
 {
 }
 
+struct RemoteApplication
+{
+	bool                    m_isConnected;
+	TcpSocket                    m_server;
+	TcpSocket                    m_client;
+	tthread::thread    *m_listeningThread;
+
+	RemoteApplication() : m_isConnected(false)
+	{
+		// start server thread
+		m_listeningThread = new tthread::thread(server, this);
+
+	}
+
+	~RemoteApplication()
+	{
+		delete m_listeningThread;
+		m_listeningThread = 0;
+	}
+
+	// play
+	// stop
+	// setTime();
+	// load( std::string operatorPath, BSONPtr data )
+	void loadOperatorGraph( /*BSONPtr operatorGraph*/ )
+	{
+		if( !m_isConnected )
+			return;
+
+		// send command to client
+		std::cout << "sending test message..." << std::endl;
+		std::string test = "huhu";
+		m_client.Send(test.c_str(), test.size());
+
+		// receive response
+		//char buffer[1000];
+		//std::cout << "sending test message..." << std::endl;
+		//int numBytesReceived = client.Receive( buffer, 10000000 );
+		//std::cout << numBytesReceived << std::endl;
+	}
+
+
+
+
+	static void server( void *args )
+	{
+		RemoteApplication *_this = (RemoteApplication *)args;
+		std::cout << "listening for client..." << std::endl;
+		_this->m_server.Bind( IpEndpointName(12345) );
+		_this->m_server.Listen();
+
+		 _this->m_server.Accept( _this->m_client );
+
+		if( _this->m_client.isValid() )
+		{
+			std::cout << "connected to client..." << std::endl;
+			_this->m_isConnected = true;
+
+			//_this->loadOperatorGraph();
+
+			// sleep until we have something to do
+			/*
+
+			// sending test message
+			std::cout << "sending test message..." << std::endl;
+			std::string test = "huhu";
+			client.Send(test.c_str(), test.size());
+
+			// receive response
+			char buffer[1000];
+			std::cout << "sending test message..." << std::endl;
+			int numBytesReceived = client.Receive( buffer, 10000000 );
+			std::cout << numBytesReceived << std::endl;
+			*/
+
+			//SOCKET_ERROR
+		}
+
+		std::cout << "closing thread" << std::endl;
+	}
+};
+
+
 
 
 
 
 int main(int argc, char ** argv)
 {
-	TcpSocket s;
-	s.Bind( IpEndpointName(12345) );
+	RemoteApplication remoteApp;
 
 	base::Application app;
 	glviewer = new base::GLViewer( 800, 600, "demo" );
